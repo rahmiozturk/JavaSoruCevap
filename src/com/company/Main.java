8301150fdf317774c8190783437fe70ef30778ef
Rahmi Öztürk <rahmi.ozturk@epias.com.tr>
2018-12-31 15:10:42 +0300

    2. soru çözüldü.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-    //Klavyeeden girilen 3 sayının ortalaması
+
+        //1. Klavyeeden girilen 3 sayının ortalaması
+
+     /*
         int x,y,z;
         double ortalama;
 
@@ -24,6 +27,40 @@ public class Main {
         ortalama=(x+y+z)/3d;
 
         System.out.println("Girilen sayıların ortalaması:" + ortalama);
+
+     */
+
+        //2. Klavyeden kenarları verilen üçgenin çeşitini bilin
+
+        int x,y,z;
+
+        Scanner tara = new Scanner(System.in);
+
+        System.out.println("1. kenarı girin");
+        x=tara.nextInt();
+
+        System.out.println("2. kenar girin");
+        y = tara.nextInt();
+
+        System.out.println("3. kenarı girin");
+        z=tara.nextInt();
+
+        if(x==y && y==z){
+        System.out.println("üçgen eşkenar");
+
+        }else if (x!=y && y!=z && x!=z) {
+        System.out.println("üçgen çeşitkenar");
+
+        }else{
+          System.out.println("üçgen ikizkenar");
+        }
+
+
+
+
+
+        }
+
     }
 
-}
+
